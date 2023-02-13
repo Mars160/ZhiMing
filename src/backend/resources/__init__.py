c3db7ac1ed7de916227884b57a32daa4ceece57d
Mars160 <wapollo9@gmail.com>
2023-02-13 22:31:34 +0800

    添加现有资源

--- src/backend/resources/__init__.py
+++ src/backend/resources/__init__.py
@@ -4,6 +4,8 @@ from .token import Token
 from .password import Password
 from .teachers import Teachers
 from .books import Books
+from .questions import Questions
+from .points import Points
 
 api = restful.Api()
 
@@ -11,3 +13,5 @@ api.add_resource(Token, '/v1/token')
 api.add_resource(Password, '/v1/password')
 api.add_resource(Teachers, '/v1/teachers/<int:tid>', '/v1/teachers')
 api.add_resource(Books, '/v1/books/<int:bid>', '/v1/books')
+api.add_resource(Questions, '/v1/questions/<int:qid>', '/v1/questions')
+api.add_resource(Points, '/v1/points/<int:pid>', '/v1/points')
