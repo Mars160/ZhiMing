6a27ea8fa7cabe76a9615958dab7a9e412418a4f
Mars160 <wapollo9@gmail.com>
2023-02-13 22:37:18 +0800

    实现students

---
+++ src/backend/resources/students.py
@@ -0,0 +1,98 @@
+from .ext import *
+
+
+class Teachers(restful.Resource):
+    @jwt_required()
+    def get(self):
+        response = response_base.copy()
+        uid = get_jwt_identity()
+
+        if not check_permission(uid, ['管理员', '教师']):
+            response['msg'] = 'permission denied'
+            response['code'] = 403
+            return response
+
+        # 获取url中的参数
+        limit = request.args.get('limit', 10, type=int)
+        page = request.args.get('page', 1, type=int)
+        students = session.query(User).filter(User.role == '学生').limit(limit).offset((page - 1) * limit).all()
+        response['data'] = {}
+        for stu in students:
+            response['data'][stu.uid] = stu.uname
+        return response
+
+    @jwt_required()
+    def post(self):
+        response = response_base.copy()
+        cur_uid = get_jwt_identity()
+        if not check_permission(cur_uid, ['管理员', '教师']):
+            response['code'] = 403
+            response['msg'] = 'permission denied'
+            return response
+
+        data = request.get_json()
+        if 'sid' in data:
+            uid = data['sid']
+        else:
+            uid = None
+        uname = data['sname']
+
+        user = session.query(User).filter(User.uid == uid).first()
+        if user is None:
+            user = User()
+            user.uid = uid
+            user.uname = uname
+            user.role = '学生'
+            user.setPassword(None)
+            session.add(user)
+            session.commit()
+            response['data'] = user.uid
+            return response
+        else:
+            response['code'] = 400
+            response['msg'] = 'user already exists'
+            return response
+
+    @jwt_required()
+    def put(self, tid: int):
+        response = response_base.copy()
+        cur_uid = get_jwt_identity()
+        if not check_permission(cur_uid, ['管理员', '教师']):
+            response['code'] = 403
+            response['msg'] = 'permission denied'
+            return response
+
+        data = request.get_json()
+        uname = data['sname']
+
+        user = session.query(User).filter(User.uid == tid).first()
+        if user is None:
+            response['code'] = 404
+            response['msg'] = 'user not found'
+            return response
+        else:
+            user.uname = uname
+            if 'pwd' in data:
+                user.setPassword(data['pwd'])
+            session.add(user)
+            session.commit()
+            return response
+
+    @jwt_required()
+    def delete(self, tid: int):
+        response = response_base.copy()
+        cur_uid = get_jwt_identity()
+        if not check_permission(cur_uid, ['管理员', '教师']):
+            response['code'] = 403
+            response['msg'] = 'permission denied'
+            return response
+
+        user = session.query(User).filter(User.uid == tid).first()
+        if user is None:
+            response['code'] = 404
+            response['msg'] = 'user not found'
+            return response
+        else:
+            session.delete(user)
+            session.commit()
+            return response
