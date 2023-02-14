5d8643bbf518240a695cdbad528861fc7e34c39f
Mars160 <wapollo9@gmail.com>
2023-02-14 23:57:15 +0800

    完善SideMenu

---
+++ src/frontend/src/components/common/SideMenu.vue
@@ -0,0 +1,73 @@
+<template>
+  <el-container>
+    <el-menu
+        v-if="show_menu"
+        style="height: 100vh"
+    >
+      <slot/>
+    </el-menu>
+    <div v-else>
+      <el-button
+          type="primary"
+          circle
+          size="large"
+          class="affix"
+          @click="show_drawer = !show_drawer"
+      >
+        <el-icon>
+          <i-ep-menu v-show="show_drawer === false"/>
+          <i-ep-close v-show="show_drawer"/>
+        </el-icon>
+      </el-button>
+      <el-drawer
+          v-model="show_drawer"
+          direction="ltr"
+          size="100%"
+          :with-header="false"
+          :show-close="false"
+      >
+        <el-menu
+            router
+            style="height: 100vh"
+        >
+          <slot/>
+        </el-menu>
+      </el-drawer>
+    </div>
+
+    <div class="m-15">
+      <router-view/>
+    </div>
+  </el-container>
+
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const show_drawer = ref(false)
+let show_menu = true
+
+//检测是否移动端
+if (window.innerWidth < window.innerHeight) {
+  show_menu = false
+}
+
+</script>
+
+<style scoped>
+.m-15 {
+  margin: 15px;
+}
+
+.affix {
+  position: fixed;
+  bottom: 100px;
+  right: 30px;
+  z-index: 2023;
+}
+
+el-menu {
+  border-right: none;
+}
+</style>
--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -2,6 +2,8 @@
 const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+const IconsResolver = require('unplugin-icons/resolver')
+const Icons = require('unplugin-icons/webpack')
 
 // const { defineConfig } = require('@vue/cli-service')
 // module.exports = defineConfig({
@@ -19,11 +21,24 @@ module.exports = {
   configureWebpack: {
     plugins: [
       AutoImport({
-        resolvers: [ElementPlusResolver()],
+        resolvers: [
+            ElementPlusResolver(),
+            IconsResolver({
+                prefix: 'Icon',
+            }),
+        ],
       }),
       Components({
-        resolvers: [ElementPlusResolver()],
+        resolvers: [
+            ElementPlusResolver(),
+            IconsResolver({
+              enabledCollections: ['ep'],
+            }),
+        ],
       }),
+        Icons({
+            autoInstall: true,
+        }),
     ],
   },
   pages: {
