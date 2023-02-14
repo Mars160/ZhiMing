c7330d051545a965f76abe87761987de7028728c
Mars160 <wapollo9@gmail.com>
2023-02-15 00:26:32 +0800

    改回router格式

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import AdminIndex from "@/views/admin/AdminIndex.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
 
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
