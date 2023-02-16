a7dbfd4b0ecfe37584620b752c616020ff2a70ca
Mars160 <wapollo9@gmail.com>
2023-02-16 22:49:48 +0800

    重命名

---
+++ src/frontend/src/views/AdminLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <SideMenu>
+    <el-menu-item index="teacher-management">
+      <el-icon>
+        <i-ep-user/>
+      </el-icon>
+      <span>教师管理</span>
+    </el-menu-item>
+  </SideMenu>
+</template>
+
+<script setup>
+import SideMenu from "@/components/common/SideMenu.vue";
+</script>
+
+<style scoped>
+.el-menu-item{
+  justify-content: center;
+}
+</style>
