ce0eebce2d6d970c6261e60ec2272bf0c4bda923
Mars160 <wapollo9@gmail.com>
2023-02-16 22:50:05 +0800

    新增两端首页

---
+++ src/frontend/src/components/common/AdminTeacherIndex.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-card>
+    <template #header>
+      <span>欢迎使用知明</span>
+    </template>
+    <span>您可以在左侧选择您希望的操作。</span>
+  </el-card>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
