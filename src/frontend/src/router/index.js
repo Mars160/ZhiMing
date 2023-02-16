32d34cc62ddf6fe2bf9675764e171ab5ac9a7e23
Mars160 <wapollo9@gmail.com>
2023-02-16 22:51:35 +0800

    根据重命名优化，合并404文件

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -1,18 +1,29 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import AdminIndex from "@/views/admin/AdminIndex.vue";
-import HelloWorld from "@/components/HelloWorld.vue";
+import AdminLayout from "@/views/AdminLayout.vue";
+import AdminTeacherIndex from "@/components/common/AdminTeacherIndex.vue";
+import NotFound from "@/components/common/NotFound.vue";
+import TeacherManagement from "@/views/admin/TeacherManagement.vue";
 
 const routes = [
   {
     path: '/admin',
-    component: AdminIndex,
+    component: AdminLayout,
     children: [
       {
-        path: 'hello',
-        component: HelloWorld,
+        path: 'index',
+        component: AdminTeacherIndex,
+        alias: ['']
       },
+      {
+        path: 'teacher-management',
+        component: TeacherManagement,
+      }
     ],
   },
+  {
+    path: '/:pathMatch(.*)*',
+    component: NotFound,
+  }
 ]
 
 const router = createRouter({
