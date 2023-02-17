6da5ad7ec402a9ca4a1d3aaab93a53b877a2e04f
Mars160 <wapollo9@gmail.com>
2023-02-17 20:35:46 +0800

    开始写老师

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -3,11 +3,27 @@ import AdminLayout from "@/views/admin/AdminLayout.vue";
 import AdminTeacherIndex from "@/components/common/AdminTeacherIndex.vue";
 import NotFound from "@/components/common/NotFound.vue";
 import UserManage from "@/views/common/UserManage.vue";
+import TeacherLayout from "@/views/teacher/TeacherLayout.vue";
 
 const routes = [
   {
     path: '/admin',
     component: AdminLayout,
+    children: [
+      {
+        path: 'index',
+        component: AdminTeacherIndex,
+        alias: ['']
+      },
+      {
+        path: 'user-manage',
+        component: UserManage,
+      }
+    ],
+  },
+  {
+    path: '/teacher',
+    component: TeacherLayout,
     children: [
       {
         path: 'index',
---
+++ src/frontend/src/views/teacher/TeacherLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <SideMenu>
+    <el-menu-item index="/admin/user-manage">
+      <el-icon>
+        <i-ep-user/>
+      </el-icon>
+      <span>学生管理</span>
+    </el-menu-item>
+  </SideMenu>
+</template>
+
+<script setup>
+import SideMenu from "@/components/common/SideMenu.vue";
+</script>
+
+<style scoped>
+
+</style>
