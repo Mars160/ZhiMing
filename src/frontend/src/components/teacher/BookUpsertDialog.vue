3ee8cae40ab28c7e1b4f7c1bdf367ac38c95d486
Mars160 <wapollo9@gmail.com>
2023-02-26 23:06:42 +0800

    取消bug fix

--- src/frontend/src/components/teacher/BookUpsertDialog.vue
+++ src/frontend/src/components/teacher/BookUpsertDialog.vue
@@ -4,14 +4,14 @@
       :on-close="onClose"
       align-center
       center
-      width="320px"
+      width="400px"
   >
     <el-row :gutter="20">
       <span class="inline-flex w-20 items-center">年级:</span>
       <el-input
           class="w-80"
           v-model="grade"
-          placeholder="请输入练习册适用年级"
+          placeholder="10代表一(上)、15代表一(下)、20代表二(上)"
       />
     </el-row>
     <el-row :gutter="20" class="mt-10">
@@ -23,6 +23,14 @@
       />
     </el-row>
     <div class="footer">
+      <el-button
+          type="danger"
+          class="mt-20"
+          @click="deleteClicked"
+          v-if="type === 'edit'"
+      >
+        删除
+      </el-button>
       <el-button
           type="primary"
           class="mt-20"
@@ -44,19 +52,20 @@
 <script setup>
 import {defineProps, inject} from 'vue'
 import axios from "axios";
-import {ElMessage} from "element-plus";
+import {ElMessage, ElMessageBox} from "element-plus";
 
 const bid = inject("select-bid")
 const bname = inject("select-bname")
 const grade = inject("select-grade")
-const show = inject("showAddUserDialog")
+const show = inject("showBookUpsertDialog");
 
 const props = defineProps({
   confirmCallback: {
     type: Function,
-    default: (bid, bname, grade, type) => {
+    default: (bid, bname, grade, type, show) => {
+      //const show = inject("showBookUpsertDialog")
       if(type === "add") {
-        axios.post('/v1/users', {
+        axios.post('/v1/books', {
           bname: bname,
           grade: grade
         }).then((res) => {
@@ -74,7 +83,7 @@ const props = defineProps({
           ElMessage.error(err)
         })
       } else if (type === "edit") {
-        axios.put('/v1/users/' + bid, {
+        axios.put('/v1/books/' + bid, {
           bname: bname,
           grade: grade
         }).then((res) => {
@@ -91,13 +100,13 @@ const props = defineProps({
           ElMessage.error(err)
         })
       }
-      this.show = false
+      show.value = false
     }
   },
   cancelCallback: {
     type: Function,
-    default: () => {
-      this.show = false
+    default: (show) => {
+      show.value = false
     }
   },
   type: {
@@ -112,11 +121,35 @@ function onClose() {
 }
 
 function confirmClicked() {
-  props.confirmCallback(bid.value, bname.value, grade.value, props.type)
+  props.confirmCallback(bid.value, bname.value, grade.value, props.type, show)
 }
 
 function cancelClicked() {
-  props.cancelCallback(bid.value, bname.value, grade.value, props.type)
+  props.cancelCallback(show)
+}
+
+function deleteClicked() {
+  ElMessageBox.confirm('此操作将永久删除该练习册, 是否继续?', '提示', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning'
+  }).then(() => {
+    axios.delete('/v1/books/' + bid.value).then((res) => {
+      const data = res.data
+      if (data.code === 0) {
+        ElMessage.success(data.msg)
+        setTimeout(() => {
+          window.location.reload()
+        }, 1000)
+      } else {
+        ElMessage.error(data.msg)
+      }
+    }).catch((err) => {
+      ElMessage.error(err)
+    })
+  }).catch(() => {
+     console.log("delete canceled")
+  })
 }
 </script>
 
--- src/frontend/src/views/teacher/BookManage.vue
+++ src/frontend/src/views/teacher/BookManage.vue
@@ -1,33 +1,57 @@
 <template>
   <ul
-    v-infinite-scroll="loadMoreBook"
-    :infinite-scroll-disabled="loadDisable"
+      v-infinite-scroll="loadMoreBook"
+      :infinite-scroll-disabled="loadDisable"
   >
-    <li v-for="book in books" :key="book.bid">
+    <li
+        v-for="book in books"
+        :key="book.bid"
+        @click="editBook(book.bid, book.bname, book.grade)"
+    >
       <el-card>
-        <span>{{book.bname}}</span>
+        <span class="book-info">{{ book.bname }}</span>
+        <span class="book-info">{{ book.gradeStr }}</span>
+      </el-card>
+    </li>
+    <li>
+      <el-card>
+        <el-button type="primary" @click="addClicked" circle size="large">
+          <el-icon>
+            <i-ep-plus/>
+          </el-icon>
+        </el-button>
       </el-card>
     </li>
   </ul>
-  <el-card>
-    <el-button type="plain" @click="addClicked" circle size="large">
-      <el-icon>
-        <i-ep-plus/>
-      </el-icon>
-    </el-button>
-  </el-card>
+
+  <BookUpsertDialog
+      :type="dialogType"
+  />
 </template>
 
 <script setup>
-import {ref} from "vue";
+import {provide, ref} from "vue";
 import axios from "axios";
 import {ElMessage} from "element-plus";
+import BookUpsertDialog from "@/components/teacher/BookUpsertDialog.vue";
 
 const books = ref([])
 const LIMIT = 10
 let PAGE = 1
 
+const dialogType = ref("add")
+
+const showBookUpsertDialog = ref(false)
+const bid = ref('')
+const bname = ref('')
+const grade = ref('')
+provide("showBookUpsertDialog", showBookUpsertDialog)
+provide("select-bid", bid)
+provide("select-bname", bname)
+provide("select-grade", grade)
+
 const loadDisable = ref(false)
+const number2Str = ['一', '二', '三', '四', '五', '六']
 
 function loadMoreBook() {
   axios.get('/v1/books', {
@@ -37,13 +61,23 @@ function loadMoreBook() {
     }
   }).then((res) => {
     const bidSet = new Set()
-    books.value.forEach((user) => {
-      bidSet.add(user.uid)
+    books.value.forEach((book) => {
+      bidSet.add(book.bid)
     })
     const result = res.data.data
-    result.forEach((user) => {
-      if (!bidSet.has(user.uid)) {
-        books.value.push(user)
+    result.forEach((book) => {
+      if (!bidSet.has(book.uid)) {
+        book.gradeStr = number2Str[parseInt(book.grade / 10)] + '年级'
+        //十位 1: 一年级, 2: 二年级, 3: 三年级, 4: 四年级, 5: 五年级, 6: 六年级 个位 0: 上册, 5: 下册
+        switch (book.grade % 10) {
+          case 0:
+            book.gradeStr += '上册'
+            break
+          case 5:
+            book.gradeStr += '下册'
+            break
+        }
+        books.value.push(book)
       }
     })
     if (result.length !== 0) {
@@ -55,13 +89,51 @@ function loadMoreBook() {
     ElMessage.error(err)
   })
 }
+
+function addClicked() {
+  dialogType.value = "add"
+  showBookUpsertDialog.value = true
+}
+
+function editBook(bid1, bname1, grade1) {
+  dialogType.value = "edit"
+  showBookUpsertDialog.value = true
+  bid.value = bid1
+  bname.value = bname1
+  grade.value = grade1
+}
 </script>
 
 <style scoped>
 .el-card {
   height: 297px;
   width: 210px;
-  line-height: 257px;
   text-align: center;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+
+  cursor: pointer;
+}
+
+.el-card:hover {
+  background-color: #f5f5f5;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 10px;
+}
+
+.book-info {
+  display: block;
+  height: 20px;
+  margin-top: 10px;
 }
 </style>
