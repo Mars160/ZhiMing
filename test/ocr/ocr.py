bdb26c7d40bb0ac6879288a3916d8d649b8fd84d
Mars160 <wapollo9@gmail.com>
2023-04-07 17:38:08 +0800

    新建测试

---
+++ test/ocr/ocr.py
@@ -0,0 +1,18 @@
+from requests import post, get
+from time import sleep
+
+path = 'http://localhost:5000/v1/plugin/ocr'
+
+req = post(path, files={'file': open('test.jpg', 'rb')})
+uuid = req.json()['data']
+print(uuid)
+
+while True:
+    req = get(path + '/' + uuid)
+    data = req.json()['data']
+    if data != None:
+        print(data)
+        break
+    else:
+        print(req.json()['msg'])
+    sleep(1)
