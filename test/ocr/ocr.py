fcb3903a3a81fbf135007311abeaa434a923e134
Mars160 <wapollo9@gmail.com>
2023-04-09 00:33:45 +0800

    服务器debug

--- test/ocr/ocr.py
+++ test/ocr/ocr.py
@@ -1,18 +1,21 @@
 from requests import post, get
 from time import sleep
 
-path = 'http://zhiming.somewang.top/v1/plugin/ocr'
+path = 'https://zhiming.somewang.top/v1/plugin/ocr'
 
 req = post(path, files={'file': open('test.jpg', 'rb')})
-uuid = req.json()['data']
-print(uuid)
+try:
+    uuid = req.json()['data']
+    print(uuid)
 
-while True:
-    req = get(path + '/' + uuid)
-    data = req.json()['data']
-    if data != None:
-        print(data)
-        break
-    else:
-        print(req.json()['msg'])
-    sleep(1)
+    while True:
+        req = get(path + '/' + uuid)
+        data = req.json()['data']
+        if data != None:
+            print(data)
+            break
+        else:
+            print(req.json()['msg'])
+        sleep(1)
+except:
+    print(req.text)
