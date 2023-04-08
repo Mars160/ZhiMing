cf12f54d627090c80f074e17622ec3417314cb43
Mars160 <wapollo9@gmail.com>
2023-04-08 22:04:43 +0800

    打包成依靠CDN分发公共库

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -4,12 +4,7 @@ const Components = require('unplugin-vue-components/webpack')
 const {ElementPlusResolver} = require('unplugin-vue-components/resolvers')
 const IconsResolver = require('unplugin-icons/resolver')
 const Icons = require('unplugin-icons/webpack')
-
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     transpileDependencies: true,
@@ -38,7 +33,36 @@ module.exports = {
             Icons({
                 autoInstall: true,
             }),
+            new HtmlWebpackPlugin({
+                filename: "index.cdn.html",
+                template: "public/index.html",
+                cdn: {
+                    vue: "https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js",
+                    "vue-router":
+                        "https://cdn.jsdelivr.net/npm/vue-router/dist/vue-router.min.js",
+                    "element-plus":
+                        "https://unpkg.com/element-plus/lib/index.full.min.js",
+                    "element-plus-icon":
+                        "https://unpkg.com/element-plus/lib/theme-chalk/el-icon.css",
+                },
+            }),
         ],
+        output: {
+            chunkFilename: 'js/[name].js',
+        },
+        externals: {
+            vue: "Vue",
+            "vue-router": "VueRouter",
+            "element-plus": "ElementPlus",
+            "element-plus-icon": "ElementPlusIcon",
+        },
+    },
+    css : {
+        extract: {
+            ignoreOrder: true,
+            filename: 'css/[name].css',
+            chunkFilename: 'css/[name].css',
+        }
     },
     pages: {
         index: {
