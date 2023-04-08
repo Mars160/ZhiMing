34cebe25feb62b468b9cd78838c0b6d816df0f1c
Mars160 <wapollo9@gmail.com>
2023-04-08 22:30:46 +0800

    修改编译后文件样式

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -4,7 +4,6 @@ const Components = require('unplugin-vue-components/webpack')
 const {ElementPlusResolver} = require('unplugin-vue-components/resolvers')
 const IconsResolver = require('unplugin-icons/resolver')
 const Icons = require('unplugin-icons/webpack')
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     transpileDependencies: true,
@@ -33,28 +32,9 @@ module.exports = {
             Icons({
                 autoInstall: true,
             }),
-            new HtmlWebpackPlugin({
-                filename: "index.cdn.html",
-                template: "public/index.html",
-                cdn: {
-                    vue: "https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js",
-                    "vue-router":
-                        "https://cdn.jsdelivr.net/npm/vue-router/dist/vue-router.min.js",
-                    "element-plus":
-                        "https://unpkg.com/element-plus/lib/index.full.min.js",
-                    "element-plus-icon":
-                        "https://unpkg.com/element-plus/lib/theme-chalk/el-icon.css",
-                },
-            }),
         ],
         output: {
             chunkFilename: 'js/[name].js',
-        },
-        externals: {
-            vue: "Vue",
-            "vue-router": "VueRouter",
-            "element-plus": "ElementPlus",
-            "element-plus-icon": "ElementPlusIcon",
         },
     },
     css : {
