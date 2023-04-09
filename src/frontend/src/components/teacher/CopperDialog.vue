24c5e9de36d438d3e04633d85e7004c3cc7a31fb
Mars160 <wapollo9@gmail.com>
2023-04-09 15:00:18 +0800

    OCR功能添加

---
+++ src/frontend/src/components/teacher/CopperDialog.vue
@@ -0,0 +1,63 @@
+<template>
+    <el-dialog title="图片剪裁" v-model="show" class="crop-dialog" append-to-body>
+        <div class="cropper-content">
+            <div class="cropper" style="text-align:center">
+                <vue-cropper
+                        ref="cropper"
+                        :img="props.img"
+
+                />
+            </div>
+        </div>
+        <div class="action-box">
+            <el-button type="primary" plain @click="clearImgHandle">清除图片</el-button>
+            <el-button type="primary" plain @click="rotateLeftHandle">左旋转</el-button>
+            <el-button type="primary" plain @click="rotateRightHandle">右旋转</el-button>
+            <el-button type="primary" plain @click="changeScaleHandle(1)">放大</el-button>
+            <el-button type="primary" plain @click="changeScaleHandle(-1)">缩小</el-button>
+            <el-button type="primary" plain @click="downloadHandle('blob')">下载</el-button>
+        </div>
+        <template #footer>
+            <el-button @click="dialogVisible = false">取 消</el-button>
+            <el-button type="primary" @click="finish" :loading="loading">确认</el-button>
+        </template>
+    </el-dialog>
+</template>
+
+<script setup>
+import {ref, reactive, inject, defineProps} from 'vue'
+import VueCropper from 'vue-cropper'
+import 'vue-cropper/dist/index.css'
+
+const show = inject('show-copper-dialog')
+const props = defineProps({
+    img: {
+        type: String,
+        required: true
+    }
+})
+
+// eslint-disable-next-line no-unused-vars
+const option = reactive({
+    img: inject('question-img-url'),
+    size: 1,
+    outputType: 'jpg',
+    full: false,
+    canMove: true,
+    canMoveBox: true,
+    original: false,
+    autoCrop: true,
+    fixed: false,
+    fixedNumber: [16, 9],
+    centerBox: true,
+    infoTrue: true,
+    fixedBox: false,
+    autoCropWidth: 100,
+    autoCropHeight: 100,
+})
+const loading = ref(false)
+</script>
+
+<style scoped>
+
+</style>
--- src/frontend/src/components/teacher/QuestionUpsertDialog.vue
+++ src/frontend/src/components/teacher/QuestionUpsertDialog.vue
@@ -22,11 +22,39 @@
             />
         </el-row>
         <el-row :gutter="20" class="mt-10">
+            <span class="inline-flex w-20 items-center">题目图片:</span>
+            <div class="w-80">
+                <el-upload action="#"
+                           drag
+                           auto-upload
+                           :before-upload="beforeImageUpload"
+                >
+                    <div v-if="new_ocr_task" style="max-height: 60px">
+                        <el-image
+                            :src="img_base64_url"
+                            fit="scale-down"
+                            style="height: 100%; width: 100%;"
+                        />
+                    </div>
+                    <div v-else>
+                        <el-icon class="el-icon-upload"><i-ep-upload-filled /></el-icon>
+                        <div class="el-upload__text">
+                            拖图片到此 或者 <em>点我上传</em>
+                        </div>
+                    </div>
+
+                </el-upload>
+            </div>
+        </el-row>
+        <el-row :gutter="20" class="mt-10">
             <span class="inline-flex w-20 items-center">题目:</span>
             <el-input
                     class="w-80"
+                    type="textarea"
                     v-model="qname"
                     placeholder="请输入题干"
+                    v-loading="ocring"
+                    :autosize="{ minRows: 2, maxRows: 7 }"
             />
         </el-row>
         <el-row class="mt-10">
@@ -73,12 +101,14 @@
             </el-button>
         </div>
     </el-dialog>
+<!--    <CopperDialog :img="img_base64_url" />-->
 </template>
 
 <script setup>
-import {defineProps, inject, nextTick, ref} from 'vue'
+import {defineProps, inject, nextTick, provide, ref} from 'vue'
 import axios from "axios";
 import {ElMessage} from "element-plus";
+// import CopperDialog from "@/components/teacher/CopperDialog.vue";
 
 const show = inject("showQuestionUpsertDialog");
 const qid = inject("select-qid");
@@ -89,7 +119,15 @@ const qplace = inject("select-qplace");
 const bid = inject("select-bid")
 
 const inputVisible = ref(false);
+const img_base64_url = ref("");
 const inputValue = ref("");
+const showCopperDialog = ref(false);
+const ocring = ref(false);
+
+const new_ocr_task = ref(false);
+
+provide("question-img-url", img_base64_url)
+provide("show-copper-dialog", showCopperDialog)
 
 const props = defineProps({
     type: {
@@ -147,6 +185,80 @@ function showInput() {
         document.getElementById("InputNewTag").getElementsByTagName("input")[0].focus();
     });
 }
+
+function beforeImageUpload(file) {
+    //判断file是否为图片
+    const fileType = file.type;
+    const validType = ['image/jpeg', 'image/png'];
+    if (!validType.includes(fileType)) {
+        ElMessage.error('上传图片只能是 JPG,PNG 格式!');
+        return false;
+    } else {
+        //转成base64
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function (e) {
+            img_base64_url.value = e.target.result;
+        }
+
+        ocring.value = true;
+        //上传图片到/v1/plugin/ocr，并获取返回的data中的uuid
+        const formData = new FormData();
+        formData.append("file", file);
+        axios.post("/v1/plugin/ocr", formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then(res => {
+            new_ocr_task.value = true;
+            const result = res.data;
+            if(result.code === 0){
+                //每秒轮询/v1/plugin/ocr/{uuid}，直到返回的data中的data不为空或undefined
+                const uuid = result.data;
+                const timer = setInterval(() => {
+                    axios.get("/v1/plugin/ocr/" + uuid).then(res => {
+                        const result = res.data;
+                        if(result.code === 0){
+                            const data = result.data;
+                            if(data !== null){
+                                new_ocr_task.value = false;
+                                clearInterval(timer);
+                                if (data.length === 0) {
+                                    ElMessage.error("未识别到文字");
+                                    ocring.value = false;
+                                    return;
+                                }
+                                //将返回的data中的data赋值给qname
+                                let str = ""
+                                for (let i = 0; i < data.length; i++) {
+                                    str += data[i];
+                                }
+                                //去除空格
+                                str = str.replace(/\s+/g, "");
+                                qname.value = str;
+                                ocring.value = false;
+                            }
+                        } else {
+                            if(new_ocr_task.value){
+                                ElMessage.error(result.msg);
+                            }
+                            ocring.value = false;
+                        }
+                    }).catch(err => {
+                        if(new_ocr_task.value){
+                            ElMessage.error(err);
+                        }
+                        ocring.value = false;
+                    })
+                }, 2000);
+            }
+        }).catch(err => {
+            ElMessage.error("上传图片失败", err);
+            ocring.value = false;
+        })
+        return false;
+    }
+}
 </script>
 
 <style scoped>
@@ -176,5 +288,10 @@ function showInput() {
 
 .mt-10 {
     margin-top: 10px;
+}
+.el-icon-upload {
+    font-size: 50px;
+    color: #99a9bf;
+
 }
 </style>
