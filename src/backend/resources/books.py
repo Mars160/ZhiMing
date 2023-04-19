5e7d106d2307bf72762a5c61f43ff8b25111d26e
Mars160 <wapollo9@gmail.com>
2023-04-19 15:42:57 +0800

    bug fix

--- src/backend/resources/books.py
+++ src/backend/resources/books.py
@@ -79,10 +79,10 @@ class Books(restful.Resource):
         rqb_a = rqbs.all()
         q_list = [rqb.qid for rqb in rqb_a]
 
-        # 删除Question表中，qid在q_list中的记录
-        db.session.query(Question).filter(Question.qid.in_(q_list)).delete(synchronize_session=False)
         # 删除RQB表中，bid为bid的记录
         rqbs.delete(synchronize_session=False)
+        # 删除Question表中，qid在q_list中的记录
+        db.session.query(Question).filter(Question.qid.in_(q_list)).delete(synchronize_session=False)
         # 删除Book表中，bid为bid的记录
         db.session.query(Book).filter(Book.bid == bid).delete(synchronize_session=False)
 
